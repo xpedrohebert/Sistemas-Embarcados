39ccd81896bca65080952e2926a88824fe677173
Pedrão <xpedrohebert@gmail.com>
2023-08-11 12:12:19 -0300

    pratica 1 parcial

---
+++ pratica1/exercicio/sample_project/main/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+
+void app_main(void)
+{
+esp_log_level_set("*", ESP_LOG_INFO);  //ENABLE ALL INFO LOGS
+
+}
