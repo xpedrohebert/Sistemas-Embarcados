1424387b651809942603d4e30563d8950ef6ae29
Pedrão <xpedrohebert@gmail.com>
2023-08-19 19:14:53 -0300

    delay

--- generic_gpio/main/gpio_example_main.c
+++ generic_gpio/main/gpio_example_main.c
@@ -127,7 +127,7 @@ void app_main(void)
     int cnt = 0;
     while(1) {
         printf("cnt: %d\n", cnt++);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
         gpio_set_level(GPIO_OUTPUT_IO_0, cnt % 2);
         gpio_set_level(GPIO_OUTPUT_IO_1, cnt % 2);
     }
