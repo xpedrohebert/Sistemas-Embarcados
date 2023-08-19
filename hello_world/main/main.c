0589b37bcf344d394c06cf9017acd9b62bd3dbbe
Pedrão <xpedrohebert@gmail.com>
2023-08-19 18:18:24 -0300

    nada

--- hello_world/main/main.c
+++ hello_world/main/main.c
@@ -42,7 +42,7 @@ void app_main(void)
 
     for (int i = 10; i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
     }
     printf("Restarting now.\n");
     fflush(stdout);
